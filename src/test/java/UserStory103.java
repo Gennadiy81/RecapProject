e42f2b60fd3e580961f82257e41159c6afe9d437
Gennadiy <79612312+Gennadiy81@users.noreply.github.com>
2021-06-30 20:28:06 -0500

    Us102 gennadiy has been completed (#2)
    * us102 begun
    
    * us102 finish
    
    * us102 made change
    
    combine 3 commits

---
+++ src/test/java/UserStory103.java
@@ -0,0 +1,7 @@
+public class UserStory103 {
+    // lots of code has been written here
+    // ready to push this branch to remote
+    // and open pull request so i can start collaborating
+    // so my team member can commit,
+    // more changes added
+}
